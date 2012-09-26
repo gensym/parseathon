ef0f734a7192377ee73f61c823aa99cc05131631
David Altenburg <david@gensym.org>
2012-09-26 18:06:22 -0500

    Added swank (old skool still)

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojars.jpoplett/fnparse "2.2.8"]
                  [slingshot "0.9.0"]]
-  :dev-dependencies [[lein-clojars "0.7.0"]])
+  :dev-dependencies [[lein-clojars "0.7.0"]]
+  :plugins [[lein-swank "1.4.1"]]
+)
